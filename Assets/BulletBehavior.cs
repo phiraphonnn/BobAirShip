d559e0908526df73f26975981a58c11de717b345
phiraphonnn <65663938+phiraphonnn@users.noreply.github.com>
2023-04-30 00:34:22 +0700

    enemy behe

--- Assets/BulletBehavior.cs
+++ Assets/BulletBehavior.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour
 {
+
+    public int damage;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +17,6 @@ public class BulletBehavior : MonoBehaviour
     {
         
     }
+    
+
 }
--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -2,37 +2,77 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    //hp
+    public int hpEnemy;
+    public int hpEnemyMax;
+    
+    //moveAi
     public float speed;
 
     public float stoppingDistance;
 
-    public float retreatDistance;
+    private float retreatDistance;
 
+    public float cooldownTime;
+    
     public Transform player;
     // Start is called before the first frame update
     void Start()
     {
+        
+     
+        retreatDistance = stoppingDistance - 1;
+        hpEnemy = hpEnemyMax;
         player = GameObject.FindGameObjectWithTag("Player").transform;
       //  stoppingDistance = Random.Range(7f, 15f);
       //  retreatDistance = Random.Range(7f, 15f);
     }
 
     // Update is called once per frame
-    void Update()
+   public void Update()
     {
-
-        if (Vector2.Distance(transform.position, player.position) > stoppingDistance)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
-            
-        }
-        else if (Vector2.Distance(transform.position,player.position) > stoppingDistance && Vector2.Distance(transform.position,player.position) > stoppingDistance)
-        {
-            transform.position = this.transform.position;
-        }
-        else if (Vector2.Distance(transform.position,player.position) < retreatDistance)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
-        }
+        AiMove();
     }
+
+   void AiMove()
+   {
+       Vector2 targetPosition = player.position + new Vector3(0f, Mathf.Sin(Time.time) * 2f, 0f); // add a sine wave to the y-position
+
+       if (Vector2.Distance(transform.position, targetPosition) > stoppingDistance)
+       {
+           transform.position = Vector2.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+       }
+       else if (Vector2.Distance(transform.position, targetPosition) < retreatDistance)
+       {
+           transform.position = Vector2.MoveTowards(transform.position, targetPosition, -speed * Time.deltaTime);
+       }
+       transform.position = new Vector2(transform.position.x, Mathf.PingPong(Time.time, 4f) - 1f);
+   }
+   
+   
+  public  void OnTriggerEnter2D(Collider2D collision)
+   {
+       if (collision.CompareTag("Bullet"))
+       {
+           BulletBehavior bullet = collision.GetComponent<BulletBehavior>();
+           HitFlashEff flashEff = gameObject.GetComponent<HitFlashEff>();
+           flashEff.Flash();
+           hpEnemy -= bullet.damage;
+           Debug.Log("Enemy hit by bullet for " + bullet.damage + " damage. HP: " + hpEnemy);
+
+           if (hpEnemy <= 0)
+           {
+               Die();
+           }
+
+           Destroy(collision.gameObject);
+       }
+   }
+
+   void Die()
+   {
+       Debug.Log("Enemy died.");
+       Destroy(gameObject);
+   }
+   
 }
